a7c66688edb81d777ce734859b3617f496a34a80
klin6 <link6@hawaii.edu>
2018-11-26 17:55:20 -1000

    don't change this keanu

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -35,15 +35,17 @@ class App extends React.Component {
                 <Route exact path="/" component={Landing}/>
                 <Route path="/signin" component={Signin}/>
                 <Route path="/signup" component={Signup}/>
-                <ProtectedRoute path="/home" component={UserHomePage}/>
                 <ProtectedRoute path="/profile" component={DisplayProfile}/>
                 <ProtectedRoute path="/profile-edit" component={EditProfile}/>
+                <ProtectedRoute path="/make-request" component={MakeRequest}/>
                 <ProtectedRoute path="/list" component={ClubDirectory}/>
+                <ProtectedRoute path="/request-list" component={RequestList}/>
                 <ProtectedRoute path="/manage" component={ManageClubAdmin}/>
                 <ProtectedRoute path="/add" component={AddClub}/>
                 <ProtectedRoute path="/club-edit/:_id" component={EditClub}/>
                 <ProtectedRoute path="/club-info/:_id" component={DisplayClub}/>
                 <AdminProtectedRoute path="/admin" component={ClubDirectorySuperAdmin}/>
+                <AdminProtectedRoute path="/request-admin" component={RequestListAdmin}/>
                 <ProtectedRoute path="/signout" component={Signout}/>
                 <Route component={NotFound}/>
               </Switch>
