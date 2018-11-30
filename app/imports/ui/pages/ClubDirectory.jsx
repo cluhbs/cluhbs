c6902a9b68f8be240492c4ac28b9dbee3d6331c3
Kathleen <kdy3@hawaii.edu>
2018-11-29 22:53:01 -1000

    Finished implementing name Search Bar

--- app/imports/ui/pages/ClubDirectory.jsx
+++ app/imports/ui/pages/ClubDirectory.jsx
@@ -1,37 +1,69 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Loader, Card, Dropdown } from 'semantic-ui-react';
+import { Container, Header, Loader, Card, Dropdown, Menu } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import ClubItem from '/imports/ui/components/ClubItem';
 // import SearchBar from '/imports/ui/components/SearchBar';
 import { Clubs } from '/imports/api/club/club';
-import { defaultInterests } from '/imports/api/profile/profile';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ClubDirectory extends React.Component {
 
   state = {
     clubs: [],
+    searchBy: 'Interest',
+    searchQuery: '',
   };
 
   constructor(props) {
     super(props);
-    this.handleClubSearchChange = this.handleClubSearchChange.bind(this);
-    this.interests = defaultInterests.map((interest, index) => ({ key: index, value: interest, text: interest }));
+    this.handleInterestSearchChange = this.handleInterestSearchChange.bind(this);
+    this.handleNameSearchChange = this.handleNameSearchChange.bind(this);
+    this.createOptions = this.createOptions.bind(this);
+    this.setSearchBy = this.setSearchBy.bind(this);
+    this.handleNameAddition = this.handleNameAddition.bind(this);
+  }
+
+  createOptions() {
+    /* eslint-disable-next-line */
+    const interestList = _.uniq(_.pluck(this.props.clubs, 'interests').flatten()).sort();
+    /* eslint-disable-next-line */
+    const clubNameList = _.uniq(_.pluck(this.props.clubs, 'name').flatten()).sort();
+    const searchByList = ['Interest', 'Name'];
+    this.interests = interestList.map((interest, index) => ({ key: index, value: interest, text: interest }));
+    this.clubNames = clubNameList.map((interest, index) => ({ key: index, value: interest, text: interest }));
+    this.searchBy = searchByList.map((interest, index) => ({ key: index, value: interest, text: interest }));
   }
 
   returnClub(clubId) {
     return Clubs.findOne({ _id: clubId });
   }
 
-  handleClubSearchChange(event, data) {
+  setSearchBy(event, data) {
     event.preventDefault();
-    const clubs = this.props.clubs.filter((x) => _.intersection(x.interests, data.value).length === data.value.length);
-    console.log(clubs);
-    console.log(_.union(_.pluck(this.props.clubs, 'interests'))); 
+    this.setState({ searchBy: data.value });
+  }
+
+  handleNameAddition(e, { value }) {
+    const clubs = this.props.clubs.filter((x) => (x.name.toUpperCase().indexOf(value.toUpperCase()) !== -1));
     this.setState({ clubs: clubs });
   }
+
+  handleInterestSearchChange(event, data) {
+    event.preventDefault();
+    /* eslint-disable-next-line */
+    const clubs = this.props.clubs.filter((x) => _.intersection(x.interests, data.value).length === data.value.length);
+    this.setState({ clubs: clubs });
+  }
+
+  handleNameSearchChange(event, data) {
+    event.preventDefault();
+    const clubs = this.props.clubs.filter((x) => (x.name.toUpperCase().indexOf(data.value.toUpperCase()) !== -1));
+    this.setState({ clubs: clubs });
+  }
+
+  handleSearchChange = (e, { searchQuery }) => this.setState({ searchQuery });
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -42,14 +74,30 @@ class ClubDirectory extends React.Component {
   renderPage() {
     const cardPadding = { padding: '30px 0px 0px 0px' };
     const contentStyle = { marginBottom: '50px' };
+    if (this.interests === undefined) {
+      this.createOptions();
+    }
     return (
         <div style={contentStyle}>
           <Container>
             <Header as="h2" dividing textAlign="center">Club Directory</Header>
             {/* <SearchBar/> */}
-            <Dropdown placeholder='Select Interests' fluid multiple search selection
-                      options={this.interests} onChange={(event, data) => this.handleClubSearchChange(event, data)}
-            />
+            <Menu>
+              <Dropdown selection defaultValue='Interest' options={this.searchBy}
+                        onChange={(e, data) => this.setSearchBy(e, data)}/>
+              {this.state.searchBy === 'Interest' ? (
+                  <Dropdown placeholder='Search by Interests' fluid multiple search selection
+                            options={this.interests} icon='search'
+                            onChange={(event, data) => this.handleInterestSearchChange(event, data)}
+                  />
+              ) : (
+                  <Dropdown placeholder='Search by Club Name' deburr fluid search selection clearable='true'
+                            options={this.clubNames} icon='search' allowAdditions
+                            onChange={(event, data) => this.handleNameSearchChange(event, data)}
+                            onAddItem={(e, data) => this.handleNameAddition(e, data)}
+                  />
+              )}
+            </Menu>
             <Card.Group style={cardPadding}>
               {this.state.clubs.length === 0 ? (
                   this.props.clubs.map((club, index) => <ClubItem key={index} club={club}/>)
